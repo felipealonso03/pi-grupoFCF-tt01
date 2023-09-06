b7911e195f67c216be8a95cd87233cae6eaeafe6
felipealonso03 <alonsolareo@udesa.edu.ar>
2023-09-06 18:49:11 -0300

    formularios y el modulo de datos

---
+++ proyectoIntegrador/data/data.js
@@ -0,0 +1,20 @@
+const moduloDatos = {
+    usuario: {
+        nombreUsuario: "nombre",
+        email: "email",
+        edad: "edad",
+        ubicacion: "lugar",
+    },
+    posteos: [
+        {
+            titulo: "titulo",
+            pieDeFoto: "piedefoto",
+            comentarios: [
+                {
+                    usuarioComentador: "nombre",
+                    textoComentario: "comentario"
+                }
+            ]
+        }
+    ]
+}
