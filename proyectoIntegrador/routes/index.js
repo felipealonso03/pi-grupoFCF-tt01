f177ac6f043ab95b8bb8f7d3c2102d3ac8808db4
cirilo2 <cbullrich@udesa.edu.ar>
2023-09-13 10:26:20 -0300

    codigo con multiples errores

--- proyectoIntegrador/routes/index.js
+++ proyectoIntegrador/routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
+const express = require('express');
 const controladorIndex = require('../controllers/indexController');
-var router = express.Router();
+const router = express.Router();
 const controlador = require("../controllers/indexController")
 
 /* GET home page. */
+
 router.get('/', controladorIndex.indexPosts);
 
 module.exports = router;
